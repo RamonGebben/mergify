5c2c93be2f84d791df6cf263f51e28c41cb0ea44
Ramon Gebben <github@ra-ge.net>
2018-04-17 11:03:59 +0200

    Added verify step and command closing #3

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const chalk = require('chalk');
 const inquirer = require('inquirer');
 const { Spinner } = require('cli-spinner');
 const pack = require('./package.json');
+const { doFetch } = require('./lib/doFetch');
 const { getMergeRequests } = require('./lib/getMergeRequests');
 const { printMergeRequest } = require('./lib/printMergeRequest');
 const { writeConfig } = require('./lib/writeConfig');
 const { readConfig } = require('./lib/readConfig');
+
+const UNAUTHORIZED_MESSAGE = '401 Unauthorized';
 
 const simplifyMergeRequest = R.pick([
   'title',
@@ -38,9 +41,16 @@ const getAllAssigned = async ({ userId }) => {
   spinner.stop();
 
   const simplifiedMergeRequest = R.map(simplifyMergeRequest)(mergeRequests);
-  simplifiedMergeRequest
-    .filter(({ state }) => state === 'opened')
-    .forEach(printMergeRequest);
+  const filteredMRs = simplifiedMergeRequest
+    .filter(({ state }) => state === 'opened');
+
+  if (filteredMRs.length) {
+    return filteredMRs
+      .forEach(printMergeRequest);
+  }
+
+  return console.log('\nNo MRs assigned to you');
+
 }
 
 const getAllSubmitted = async ({ userId }) => {
@@ -52,9 +62,36 @@ const getAllSubmitted = async ({ userId }) => {
   const mergeRequests = await getMergeRequests(params);
   spinner.stop();
   const simplifiedMergeRequest = R.map(simplifyMergeRequest)(mergeRequests);
-  simplifiedMergeRequest
-    .filter(({ state }) => state !== 'merged')
-    .forEach(printMergeRequest);
+  const filteredMRs = simplifiedMergeRequest
+    .filter(({ state }) => state !== 'merged');
+
+  if (filteredMRs.length) {
+    return filteredMRs
+      .forEach(printMergeRequest);
+  }
+
+  return console.log('\nNo MRs ready for review');
+}
+
+const verify = async ({ userId }) => {
+  try {
+    console.log('Verifying your config');
+    spinner.start();
+    const userResp = await doFetch(`/users/${userId}`);
+
+    if (R.path(['message'], userResp) === UNAUTHORIZED_MESSAGE) {
+      throw new Error(UNAUTHORIZED_MESSAGE);
+    }
+
+    spinner.stop();
+    console.log('\n🎉  All set, your ready to mergify!');
+    return process.exit(0);
+  } catch (error) {
+    spinner.stop();
+    console.log('\n🙀  Oh no, could not complete verify. Please review your config');
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 const configure = async () => {
@@ -77,6 +114,8 @@ const configure = async () => {
         message: 'On what domain is your Gitlab instance?'
       }
     ]);
+
+    await verify(answers);
 
     return writeConfig(answers);
   } catch (error) {
@@ -105,6 +144,11 @@ const options = [
     description: 'Setup or update required config',
     fn: configure
   },
+  {
+    trigger: '-v --verify',
+    description: 'Verify your config is correct',
+    fn: verify
+  },
 ];
 
 const run = async() => {
@@ -118,7 +162,7 @@ const run = async() => {
   if (!userId || !privateToken) {
     await configure();
     config = await readConfig();
-    console.log('🎉  All set, your ready to mergify!');
+    await verify();
   }
 
   options.forEach(({ trigger, description, fn }) => {
